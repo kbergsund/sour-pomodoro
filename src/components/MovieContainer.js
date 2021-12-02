efc48e974a5d1ceddd30de18090e76dcb0a7d68d
Kyra Bergsund <k.bergsund@gmail.com>
2021-12-02 14:08:27 -0800

    Define App components, add initial content

--- src/components/MovieContainer.js
+++ src/components/MovieContainer.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 import movieData from '../movieData'
 
+class MovieContainer extends Component {
+  constructor() {
+    super();
+  }
 
+  render() {
+    return (
+      <p>I am the container</p>
+    )
+  }
+}
 
 
 

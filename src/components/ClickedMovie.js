1300ec2642d7ab3d89d6baa7f8cdff4a88ea1dbc
Kyra Bergsund <k.bergsund@gmail.com>
2021-12-03 07:53:28 -0800

    Begin ClickedMovie styling

--- src/components/ClickedMovie.js
+++ src/components/ClickedMovie.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../scss/ClickedMovie.scss'
 
 const ClickedMovie = ({ clicked, handleClick }) => {
   return (
@@ -7,14 +8,16 @@ const ClickedMovie = ({ clicked, handleClick }) => {
         type="button"
         onClick={(event) => handleClick(event)}>x</button>
       <img src={clicked['backdrop_path']} alt="movie backdrop"></img>
-      <h2>{clicked.title}</h2>
-      <p>Rating: {clicked['average_rating']}</p>
-      <p>Release Year: {clicked['release_date']}</p>
-      <p>Overview: {clicked.overview}</p>
-      <p>Revenue: {clicked.revenue}</p>
-      <p>Budget: {clicked.budget}</p>
-      <p>Runtime: {clicked.runtime}</p>
-      <p>Tagline: {clicked.tagline}</p>
+      <section className="movie-stats">
+        <h2>{clicked.title}</h2>
+        <p>Rating: {clicked['average_rating']}</p>
+        <p>Release Year: {clicked['release_date']}</p>
+        <p>Overview: {clicked.overview}</p>
+        <p>Revenue: {clicked.revenue}</p>
+        <p>Budget: {clicked.budget}</p>
+        <p>Runtime: {clicked.runtime}</p>
+        <p>Tagline: {clicked.tagline}</p>
+      </section>
     </article>
   )
 }

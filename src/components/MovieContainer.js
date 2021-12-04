9abe409c50ca038fbb71f19866a6a174529d9c85
Kyra Bergsund <k.bergsund@gmail.com>
2021-12-04 11:54:51 -0800

    Update loading/error message styling, refactor

--- src/components/MovieContainer.js
+++ src/components/MovieContainer.js
@@ -36,21 +36,19 @@ class MovieContainer extends Component {
 
   handleError = () => {
     if (this.state.networkErr.message === '500') {
-      return <h1>A server error occured, super bummer :/ Try again later</h1>
-    }
-    else {
-      return <h1>An unkwown error occured, can\'t help ya🤷‍♀️</h1>
+      return <h2>A server error occured, super bummer :/ Try again later</h2>
+    } else {
+      return <h2>An unkwown error occured, can\'t help ya there 🤷‍♀️</h2>
     }
   }
 
   render() {
-
-    const allMovies = !this.state.isLoaded ? <h1>Loading...</h1> : this.state.movieData.map(movie => {
+    const allMovies = !this.state.isLoaded ? <h2>Loading...</h2> : this.state.movieData.map(movie => {
       return <Movie key={movie.id} poster={movie['poster_path']} handleClick={this.handleClick} />
     })
 
     return (
-      <main className="movie-container">
+      <main>
         {this.state.networkErr ? this.handleError() :
           !this.state.clickedMovie ? allMovies : <ClickedMovie clicked={this.state.clickedMovie} handleClick={this.handleClick} />}
       </main>

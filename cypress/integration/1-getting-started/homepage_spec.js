d2f0dfc80d7ea3fbcd606e940942272b004b810e
Regan Losey <regan.losey@gmail.com>
2021-12-07 14:42:49 -0700

    Install Cypress and write sample test

---
+++ cypress/integration/1-getting-started/homepage_spec.js
@@ -0,0 +1,9 @@
+describe('Homepage', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('should have a sample test', () => {
+    expect(true).to.equal(true)
+  })
+})

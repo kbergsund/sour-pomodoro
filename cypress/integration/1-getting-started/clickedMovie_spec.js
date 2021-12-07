bc85775e39d198db0418c695a96f8334f3988de5
Regan Losey <regan.losey@gmail.com>
2021-12-07 16:41:52 -0700

    Write and pass test to intercept clickedMovie request

--- cypress/integration/1-getting-started/clickedMovie_spec.js
+++ cypress/integration/1-getting-started/clickedMovie_spec.js
@@ -1,11 +1,47 @@
 describe('Clicked movie', () => {
   beforeEach(() => {
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 200,
+      body: {
+        movies:
+          [
+            {
+              id: 694919,
+              poster_path: "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg"
+            },
+            {
+              id: 337401,
+              poster_path: "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg"
+              }
+          ]
+      }
+    })
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', {
+      statusCode: 200,
+      body: {
+        movie:
+            {
+              id: 694919,
+              title: "Money Plane",
+              "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+              "release_date": "2020-09-29",
+              overview: "A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",
+              budget: 0,
+              revenue: 0,
+              runtime: 82,
+              tagline: "",
+              "average_rating": 6.625
+              }
+      }
+    })
     cy.visit('http://localhost:3000/')
     .get('main')
     .get('article')
     .get(':nth-child(1) > .poster')
     .click()
   })
+
+
   it('should have a sample test', () => {
     expect(true).to.equal(true)
   });

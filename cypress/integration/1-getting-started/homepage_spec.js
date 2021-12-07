4bc2c5b86aa3ae8746f29c84b81a0f8482e2ce9c
Regan Losey <regan.losey@gmail.com>
2021-12-07 15:08:29 -0700

    Write and pass tests for iteration 1

--- cypress/integration/1-getting-started/homepage_spec.js
+++ cypress/integration/1-getting-started/homepage_spec.js
@@ -5,5 +5,21 @@ describe('Homepage', () => {
 
   it('should have a sample test', () => {
     expect(true).to.equal(true)
-  })
+  });
+
+  it('should display the site header, containing name and background image', () => {
+    cy.get('header')
+    .should('have.css', 'background')
+    .and('include', 'linear-gradient')
+    .and('include', 'url')
+    .get('h1')
+    .contains('sour pomodoro')  
+  });
+
+  it('should display movie posters inside MovieContainer', () => {
+    cy.get('main')
+    .get('article')
+    .get('img')
+  });
+
 })

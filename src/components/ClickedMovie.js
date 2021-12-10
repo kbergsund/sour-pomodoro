499d3f0e7838a0b6b26fdb9b65a21e80962af5af
Kyra Bergsund <k.bergsund@gmail.com>
2021-12-10 13:13:18 -0800

    Implement method for handling unavailable data on certain clicked movies

--- src/components/ClickedMovie.js
+++ src/components/ClickedMovie.js
@@ -25,11 +25,15 @@ class ClickedMovie extends React.Component {
     }))
   }
 
-  // handleUnavailableData = (number) => {
-  //   if (!number) {
-  //     console.log('hi')
-  //   }
-  // }
+  handleUnavailableData = (data) => {
+    if (!data) {
+      return 'N/A'
+    } else if (parseInt(data)) {
+      return `$${new Intl.NumberFormat().format(data)}`
+    } else {
+      return data
+    }
+  }
   
   render() {  
     const displayCurrentMovie = this.state.clickedMovieNetworkErr ? this.props.handleError(this.state.clickedMovieNetworkErr) : !this.state.isLoaded ? <h1>Loading...</h1> :
@@ -40,10 +44,10 @@ class ClickedMovie extends React.Component {
           <h3>{this.state.currentMovie.title}</h3>
           <p>Rating: {Math.round(this.state.currentMovie['average_rating'] * 10)/10}/10</p>
           <p>Release Year: {new Date(this.state.currentMovie['release_date']).toLocaleDateString()}</p>
-          <p>Revenue: $ {new Intl.NumberFormat().format(this.state.currentMovie.revenue)}</p>
-          <p>Budget: $ {new Intl.NumberFormat().format(this.state.currentMovie.budget)}</p>
+          <p>Revenue: {this.handleUnavailableData(this.state.currentMovie.revenue)}</p>
+          <p>Budget: {this.handleUnavailableData(this.state.currentMovie.budget)}</p>
           <p>Runtime: {this.state.currentMovie.runtime} minutes</p>
-          <p>Tagline: {this.state.currentMovie.tagline}</p>
+          <p>Tagline: {this.handleUnavailableData(this.state.currentMovie.tagline)}</p>
           <p>Overview: {this.state.currentMovie.overview}</p>
         </div>
       </section>

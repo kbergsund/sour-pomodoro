d91fa0f9b55ba3c7cff71d3c6d3e29d64ee4980b
Regan Losey <regan.losey@gmail.com>
2021-12-10 13:58:31 -0700

    Complete error handling tests and organize testing files

--- cypress/integration/errorHandle_spec.js
+++ cypress/integration/errorHandle_spec.js
@@ -3,14 +3,8 @@ describe('Errors', () => {
     cy.visit('http://localhost:3000/banana')
       .get('.movie-container')
       .contains('p', 'Uhh, u lost? 404 - Invalid URL')
-    })
-
-    it('should be able to catch a 500 level error', () => {
-    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v1/movies', {
-      statusCode: 500,
-    })
-      .request({url:'https://rancid-tomatillos.herokuapp.com/api/v1/movies',failOnStatusCode: false})
-      // .get('.movie-container')
-      // .contains('h1', 'An unknown error occured, can\'t help ya there 🤷‍♀️')
+    cy.visit('http://localhost:3000/movies/banana')
+      .get('.movie-container')
+      .contains('p', 'Uhh, u lost? 404 - Invalid URL')
     })
 })

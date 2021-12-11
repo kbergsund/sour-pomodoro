0e2ed2bfd7e567c872af3d3e186a113809324833
Regan Losey <regan.losey@gmail.com>
2021-12-11 12:09:49 -0700

    Adjust positioning of text box and wrap image in div

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -5,17 +5,19 @@ import '../scss/ErrorPage.scss'
 
 const ErrorPage = () => {
   return (
-    <div className="image-container">
-      <h1 className="top-left invalid">404-invalid URL</h1>
-      <p className="centered spooky">Whoa...kinda spooky in here</p>
-      <Link to="/">
-        <p className="link"> Get me outta here!</p>
+    <div className="container">
+      <img
+        className="theater-img"
+        src="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        alt="an empty movie theater">
+      </img>
+      <div className="text-block">
+        <h1 className="invalid">404-invalid URL</h1>
+        <p className="spooky">Whoa...kinda spooky in here</p>
+        <Link to="/">
+          <p className="link"> Get me outta here!</p>
         </ Link>
-        <img
-          className="theater-img"
-          src="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-          alt="an empty movie theater">
-        </img>
+      </div>
     </div>
   )
 

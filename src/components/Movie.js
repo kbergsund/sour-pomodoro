098a2ebb4f16dd8ceb5f4b787242f7db0e125631
Regan Losey <regan.losey@gmail.com>
2021-12-12 11:48:47 -0700

    Integrate simple star ratings

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../scss/Movie.scss'
 import { Link } from 'react-router-dom';
+import Star from 'simple-rating-stars'
 
-const Movie = ({ id, poster }) => {
+const Movie = ({ id, poster, rating }) => {
   return (
-    <article>
+    <article className="movie">
       <Link to={`/movies/${id}`}>
         <img
           className="poster"
@@ -12,6 +13,13 @@ const Movie = ({ id, poster }) => {
           alt="movie poster">
         </img>
       </Link>
+        <Star
+        stars={Math.round(rating)}
+        outOf={10}
+        full={'#ffffff'} 
+        empty={'##000000'}
+        stroke={'#a33c00'}
+        />
     </article>
   )
 }
